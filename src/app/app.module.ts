667c5e47dc01b00ceba737408b4ce092bb007e5a
danverde <verde.daniel.d@gmail.com>
2022-07-26 21:54:04 -0400

    - added redux to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { ButtonComponent } from './components/common/button/button.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,14 @@ import { ButtonComponent } from './components/common/button/button.component';
     ButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
